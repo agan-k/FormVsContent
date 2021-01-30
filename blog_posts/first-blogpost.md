ef3bc55ce3ce6cfb0dc5386a88b942c9b6ca8938
Koran Agan <koranagan@gmail.com>
2021-01-30 13:44:19 -0500

    add content to React intro post

--- blog_posts/first-blogpost.md
+++ blog_posts/first-blogpost.md
@@ -5,14 +5,31 @@ date: "January 2021 by"
 author: 'K. Agan'
 ---
 
+When I first started out getting into [React](https://reactjs.org/) library (framework)
+I already thought I had a decent sense for what DOM is. I could also write few lines of javascript
+to manipulate the HTML in the DOM and then style it with some CSS and this is how I was able to build simple
+websites. This is how one can build anything that a browser is capable of rendering, really. 
+And the experienced ones, who could build anything this way, decided to take it a bit further by building 
+frameworks and libraries in order to increase productivity and enhance performance. I finally arrived at the point
+where I could no longer avoid dealing with these frameworks. I took a course in React, started building with this
+new tool and never looked back.
 
+However, my transition to React was not exactly smooth and, by sharing my experience here, I'm hoping to provide some insight into certain ways of thinking which might be helpful. 
 
 ## How to do stuff. h2
 
-- This and
-- That
-- Will be
-- done
+It is really easy to jump right into it and create a React app in the command line that will right out of the box do stuf for us. This is how I started off and it was somewhat counter productive. To be clear, this is not to say that getting your heands dirty with a new concept and figuring the parts out by yourself is not a valid way to approach things. On contrary, this is how I operate most of the time. However, I also noticed that, had I had some missing pieces of information beforehand, this process would have been much easier. So, jumping straight into the 'tutorial hell' (as one of my instructors called it) to obtain some arbitrary solution for our project, was not exactly the best way to do things in my own experience. Tutorials (even the mediocare ones) are a tremendous resource and absolutely essential for our ever changing ecosystem but a certain ballance in our approach needs to be struck so we can achive an optimal growth in our knowledge. 
+
+So before going any further, here is a short (bare minimum) list which contain resouces that are very simple but essential to understanding how React works
+
+- DOM (HTML)
+- basic data types (javascript)
+- conditionals (javascript)
+- logical operators (javascript)
+- loops (javascript)
+- functions (javascript)
+
+
 
 ### This h3
 
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -9,9 +9,8 @@ const CodeBlock = ({ language, value }) => {
  };
 
 export default function Post({ content, data }) {
-  // This holds the data between `---` from the .md file
+
    const frontmatter = data
-   console.log(frontmatter)
 
   return (
      <Layout>
@@ -26,18 +25,17 @@ export default function Post({ content, data }) {
                   <h1>{frontmatter.title}</h1>
                </div>
                <span className='date'>{frontmatter.date}</span>&nbsp;<span className='author'>{frontmatter.author}</span>
-               <ReactMarkdown
-                  escapeHtml={false}
-                  source={content}
-                  renderers={{ code: CodeBlock }}
-               />
-              
+               <div className='post-body'>
+                  <ReactMarkdown
+                     escapeHtml={false}
+                     source={content}
+                     renderers={{ code: CodeBlock }}
+                  />
+               </div>
             </div>
         </div>
         <style jsx>{`
             .container {
-               // display: flex;
-               // justify-content: space-around;
                color: #495057;
                font-family: -apple-system,BlinkMacSystemFont,Helvetica Neue,Roboto,
                Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI,Segoe UI Emoji,Segoe UI Symbol,
@@ -45,26 +43,13 @@ export default function Post({ content, data }) {
             }
             .post-list {
                width: 22%;
-               // background-color: white;
             }
-              
             .post-container {
-               width: 80%;
+               width: 100%;
                // padding: 1rem;
                margin: 5rem auto;
                text-align: initial;
                // background-color: white;
-            }
-            .title-image {
-               display: flex;
-            }
-            .title-image img {
-               height: 4rem;
-               border-radius: 15%;
-               transform: rotate(-8deg);
-               margin-left: -1rem;
-               box-shadow: 3px 2px 5px 0px grey;
-               border: 2px solid rgb(240, 226, 202);
             }
             .post-container h1 {
                font-size: 2rem;
@@ -77,10 +62,28 @@ export default function Post({ content, data }) {
             }
             .date {
                font-style: italic;
-               margin-left: 4rem;
+               margin-left: 6rem;
             }
             .author {
                font-weight: 700;
+            }
+            .title-image {
+               display: flex;
+               margin-left: 2rem;
+            }
+            .title-image img {
+               height: 4rem;
+               border-radius: 15%;
+               transform: rotate(-8deg);
+               margin-left: -1rem;
+               box-shadow: 3px 2px 5px 0px grey;
+               border: 2px solid rgb(240, 226, 202);
+            }
+            .post-body {
+               margin-left: 6rem;
+            }
+            .post-body p a {
+               color: red;
             }
             
          `}</style>
@@ -92,13 +95,13 @@ export default function Post({ content, data }) {
 Post.getInitialProps = async (context) => {
    const { slug } = context.query
  
-   // Import our .md file using the `slug` from the URL
+   // Import .md file using the `slug` from the URL
    const content = await import(`../../blog_posts/${slug}.md`)
 
    // Parse .md data through `matter`
    const data = matter(content.default)
 
-   // Pass data to our component props
+   // Pass data to component props
    return { ...data }
  
    return { slug }
