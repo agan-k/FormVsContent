3a97b2d7363c49dac6e8309741b7fd5ebb22b4aa
Koran Agan <koranagan@gmail.com>
2021-05-11 16:25:54 -0400

    update about

--- pages/about.js
+++ pages/about.js
@@ -7,77 +7,91 @@ export default function About() {
    return (
       <Layout>
          <div className={style.container}>
-            <Badge />
-               <div className={style.resume_wrapper}>
-                  <p><strong>Summary</strong>
-                     <br />
-                     <br />
-                     I am a detail-oriented Software Engineer who holds over 15 years of experience in music industry and music 
-                     education. My background helps me translate abstract ideas into simplistic and comprehensive symbolic 
-                     language and for the past several years I have used these skills to help a diverse group of clients improve 
-                     their online presence. I leverage my experience as both creator and educator when producing high-quality and 
-                     intuitive products with real users in mind.
+            {/* <Badge /> */}
+            <div className={style.resume_wrapper}>
+               <h4>Summary</h4>
+                  <p>
+                     Detail-oriented Software Engineer delivering UX /UI design and content development across the entertainment, hospitality, and
+                     education industries. Specialized in translating client vision into actionable suggestions, UX roadmaps, and a unique user-journey
+                     that grows brands and recognition. Experienced front end developer collaborating with stakeholders and design teams to create a
+                     tailored customer journey to lead users through improved experiences and deep understanding of the brand.
                   </p>
-                  <br/><br/>
                   <p><strong>Languages, Frameworks, Skills ...</strong>
                      <br />
-                     <br />
-                     HTML5, CSS3, JavaScript, React, Next, Express, PostgreSQL, MongoDB, Ruby, RESTful API, Headless CMS, others  
+                     HTML5, CSS3, JavaScript, React.JS, Next.JS, Express, PostgreSQL, MongoDB, Ruby, RESTful API, Headless CMS, others  
                   </p>
                   <br/>
-                  <p><strong>Experience</strong>
-                     <br /><br />
-                     FORM VERSUS CONTENT (2018 - present)<br/>
-                     <em>Independent Contractor - Web Developer/Software Engineer</em><br />
-                     <ul>
-                        <li>
-                           Worked closely with clients to design user friendly solutions for their online presentations and content
-                           distributions, while leveraging the latest software engineering tools.
-                        </li>
-                        <li>
-                           Experience in client facing skills, UX/UI design and content development/writing.
-                        </li>
-
-                     </ul>
-                     <br/>
-                     MUSICIAN (2002 - 2018)<br />
-                     <em>Performer, Composer and Educator</em><br />
-                     <ul>
-                        <li>Toured internationally and performed major music festivals</li>
-                        <li>Worked as session musician and composed music for film</li>
-                        <li>Gave masterclasses, workshops and published educational materials</li>
-                     </ul>
-                     <br/>
-                     MICRON TECHNOLOGY (1998)<br />
-                     <em>Facilities Instrumentation and Controls Technician</em><br />
-                     <ul>
-                        <li>Service, maintenance, modification and repair of instrumentation and controls
-                           systems components for a state-of-the-art semiconductor FAB</li>
-                        <li>High level of service with strong customer focus and build effective relationships
-                           with supported teams
-                        </li>
-                     </ul>
-                  </p>
-                  <br/>
+               <h4>Experience</h4>
+                  <p className={style.job_title}>Web Developer/Software Engineer</p>
                   <p>
-                     <strong>Education</strong>
-                     <br /><br />
+                  <em>Contract, FORM VS CONTENT Studio (2018 - present) </em><br/>
+                  Worked closely with clients to design user friendly solutions for their online presentations and content
+                  distributions, while leveraging the latest software engineering tools. <br/>
+                     <ul>
+                        <li>
+                        Communicate and liaise with client facing teams and decision makers, delivering UX/UI design, content
+                        development and content writing to enhance brand voice.
+                        </li>
+                        <li>
+                        Created marketing website with CMS to increase exposure via online presence by developing concept and
+                        design of web application to distribute and sell merchandise, integrated into third-party online store.
+                        </li>
+                        <li>
+                        Designed an interactive music education learning and development application modeled on movable do solfège;
+                        incorporated training and testing methods for upskilling end-users, project leveraged HTML5, CSS and Javascript
+                        technologies to develop engaging and accessible user experience
+                        </li>
+                        <li>
+                        Identified opportunity to leverage Art Institute of Chicago public API to create a responsive image gallery
+                        for 3 different browsers by extracting image data and centralizing access.
+                        </li>
+                     
+                     </ul>
+               </p>
+               <p className={style.job_title}>Performer, Composer and Educator</p>
+                  <p>
+                  <em>Music, Multiple Brands and Events, (2002 - 2018)</em><br />
+                  Toured internationally and performed major music festivals, collaborating with production companies and brands. 
+                     <ul>
+                        <li>Worked as session musician and composed music for film</li>
+                        <li>Taught masterclasses and workshops to lead aspiring professionals
+                            through fundamentals, advanced training, and continuing education topics.</li>
+                        <li>Developed and composed several educational materials for publishing outlets and book formats.  </li>
+                     </ul>
+                  </p>
+                   
+                  <p className={style.job_title}>Facilities Instrumentation and Controls Technician</p>
+                  <p>
+                  <em>MICRON TECHNOLOGY (1998)</em><br />
+                  Charged with overseeing full project scope of work, defining maintenance, modification, and repair
+                  protocols for instrument control systems components.
+                     <ul>
+                        <li>Maintained workflows across the entire process, managed issue and project log to communicate
+                        status with the supervisor.
+                        </li>
+                        <li>Managed testing process communication with other team members for timed and seamless
+                           delivery, focused on strong customer focus and build effective relationships with cross-functional teams.
+                        </li>
+                     </ul>
+                  </p>
+                  <br/>
+                  
+                  <h4>Education</h4>
+                  <p>
                      GENERAL ASSEMBLY, NEW YORK, NY<br/>
                      <em>Software Engineering Immersive</em>
-                     <br /><br />
+                     <br />
                      BERKLEE COLLEGE OF MUSIC<br/>
                      <em>BM - Performance/Composition</em>
-                     <br /><br />
+                     <br />
                      EL. ENGINEERING VOCATIONAL, <em>(Bosnia Herzegovina)</em><br/>
                      <em>Associate's degree equivalent</em>
                      <br />
-                     <br />
-                     <br />
-                     <strong>Other interests</strong>
-                     <br />
-                     <br />
+                  </p>
+                  <br/>
+                  <h4>Other interests</h4>
+                  <p>
                      Blogging, Photography, Music 
-                     <br />
                      <br />
                      <strong>Fluent in:</strong>&nbsp;
                      English, German, Bosnian <em>(Croatian, Serbian)</em>
--- pages/index.js
+++ pages/index.js
@@ -44,6 +44,16 @@ export default function Home() {
             
             <h1>&#123;current projects&#125;</h1>
          <Project
+         name='RajivJayaveera.com'
+         url='https://rajivJayaweera.com'
+         stack={rajStack}
+         repo_link='https://github.com/agan-k/raj'
+         desktop_sshot={['/assets/screen_shots/raj_desktop.jpg', '/assets/screen_shots/raj_desktop1.jpg']}
+         // tablet_sshot={['', '']}
+         mobile_sshot={['/assets/screen_shots/raj_mobile.jpg', '/assets/screen_shots/raj_mobile1.jpg',
+         '/assets/screen_shots/raj_mobile2.jpg', '/assets/screen_shots/raj_mobile3.jpg',]}
+         />
+         <Project
             name='slo͞oh.app'
             url='https://slooh.app'
             stack={sloohStack}
@@ -62,16 +72,6 @@ export default function Home() {
             mobile_sshot={['', '']}
          />
          <Project
-            name='RajivJayaveera.com'
-            url='https://rajivJayaweera.com'
-            stack={rajStack}
-            repo_link='https://github.com/agan-k/raj'
-            desktop_sshot={['/assets/screen_shots/raj_desktop.jpg', '/assets/screen_shots/raj_desktop1.jpg']}
-            // tablet_sshot={['', '']}
-            mobile_sshot={['/assets/screen_shots/raj_mobile.jpg', '/assets/screen_shots/raj_mobile1.jpg',
-            '/assets/screen_shots/raj_mobile2.jpg', '/assets/screen_shots/raj_mobile3.jpg',]}
-         />
-         <Project
             name='AIC-Photography.info'
             url='https://aic-photography-collection.info'
             stack={aicStack}
@@ -80,7 +80,7 @@ export default function Home() {
             tablet_sshot={['/assets/screen_shots/aic_tablet.png']}
             mobile_sshot={['/assets/screen_shots/aic_mobile.png', '/assets/screen_shots/aic_mobile1.png',]}
          />
-         <Project
+         {/* <Project
             name='KatarinaOlujic.com'
             url='https://katarina-olujic.netlify.app'
             stack={katStack}
@@ -97,7 +97,7 @@ export default function Home() {
             desktop_sshot={['/assets/screen_shots/dom_desktop.jpg']}
             tablet_sshot={['/assets/screen_shots/dom_tablet.jpg']}
             mobile_sshot={['/assets/screen_shots/dom_mobile.jpg', '/assets/screen_shots/dom_mobile1.jpg',]}
-         />
+         /> */}
       </div>
    </Layout>
   )
