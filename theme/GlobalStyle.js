4f4d1b655eda70c84d8da5401f3379af3ddb923c
agan-k <koranagan@gmail.com>
2023-11-02 09:02:34 -0400

    adjust css transitions

--- theme/GlobalStyle.js
+++ theme/GlobalStyle.js
@@ -3,13 +3,15 @@ import {theme} from "./theme"
 
 export const GlobalStyles = createGlobalStyle({
 
+  '*': {
+    transition: 'all 0.50s linear',
+  },
   body: {
     maxWidth: '1200px',
     margin: '0 auto',
     background: ({ theme }) => theme.colors.background,
     color: ({ theme }) => theme.colors.text,
     fontFamily: 'Tahoma, Helvetica, Arial, Roboto, sans-serif',
-    transition: 'all 0.50s linear',
   },
   a: {
     textDecoration: 'none',
