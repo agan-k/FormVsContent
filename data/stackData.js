69650e7ea5e023b74fb29ff657fee904a1f4f720
agan-k <koranagan@gmail.com>
2023-11-02 09:04:22 -0400

    update stack data css

--- data/stackData.js
+++ data/stackData.js
@@ -1,6 +1,6 @@
 const stackData = [
    { id: 1, name: 'HTML5', src: '/assets/html5.png' },
-   { id: 2, name: 'CSS3', src: '/assets/css3.png' },
+   { id: 2, name: 'CSS', src: '/assets/css3.png' },
    { id: 3, name: 'JavaScript', src: '/assets/javascript.png' },
    { id: 4, name: 'ReactJS', src: '/assets/reactJS.png' },
    { id: 5, name: 'NextJS', src: '/assets/nextJS.png' },
