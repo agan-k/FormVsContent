f5c0f9ec70996d63500ddd883b0c5574b1f46730
agan-k <koranagan@gmail.com>
2023-12-24 16:20:08 -0500

    add curly brackets to the site logo

--- components/badge/Badge.js
+++ components/badge/Badge.js
@@ -1,12 +1,15 @@
 import {SiteLogo} from "../site-logo";
 import {theme as staticTheme} from '../../theme'
-import {Container, LinksContainer} from "./styled";
+import { CurlyBrackets } from "../curly-brackets";
+import {Container} from "./styled";
 
 
 export default function Badge() {
   return(
     <Container>
+      <CurlyBrackets size={staticTheme.space[6]}>
         <SiteLogo size={[staticTheme.space[6]]}/>
+      </CurlyBrackets>
     </Container>
   );
 }
