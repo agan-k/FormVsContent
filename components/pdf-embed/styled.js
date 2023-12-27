cbb84b71f028c4b7bf059f2166d37b41b4d2f557
agan-k <koranagan@gmail.com>
2023-12-27 13:32:17 -0500

    fix hight o pdf wrapper

--- components/pdf-embed/styled.js
+++ components/pdf-embed/styled.js
@@ -4,7 +4,7 @@ import {theme as staticTheme} from '../../theme';
 export const PdfWrapper = styled('div')(
   {
     object: {
-      height: ({isOpenPDF}) => isOpenPDF ? '100Vh' : staticTheme.space[3],
+      height: ({isOpenPDF}) => isOpenPDF ? '100Vh' : `${staticTheme.space[7]}px`,
     },
   }
 );
