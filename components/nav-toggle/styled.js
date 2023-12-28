31d7038bc322634f1b949be4294edc6ccbe2531d
agan-k <koranagan@gmail.com>
2023-12-28 00:59:02 -0500

    adjust nav togg;e placement for safari

--- components/nav-toggle/styled.js
+++ components/nav-toggle/styled.js
@@ -3,6 +3,7 @@ import {theme as staticTheme} from '../../theme';
 
 export const NavToggleButton = styled('button')(
   {
+    width: staticTheme.space[4],
     border: 'none',
     background: 'none',
     fontSize: staticTheme.fontSizes[5],
