163c310dbf8a17ca41c9671c446bf9633c87c9e5
agan-k <koranagan@gmail.com>
2024-01-25 17:38:33 -0500

    update config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-module.exports = {
-   webpack: function(config) {
-     config.module.rules.push({
-       test: /\.md$/,
-       use: 'raw-loader',
-     })
-     return config
-   }
- }
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
